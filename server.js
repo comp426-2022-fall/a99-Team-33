dfc5a96fe8b0c1069e00e8aef46022119db4f6a9
TimG233 <gaosh0830@gmail.com>
2022-12-07 23:37:15 -0500

    Added sqlite3 database | Add loggings to database | Added logs related endpoints

--- server.js
+++ server.js
@@ -7,11 +7,14 @@
  * This file should use "export function" from covid-api.js to retrieve data.
  * 
  * return stringify json data if user uses --j flag.
+ * 
+ * This file should use method in "module.exports" from sql-operations.js to retrieve data.
  */
 
 import express from 'express';
 import { globalAll, globalYesterdayAll, countryCovidData, countryYesterdayCovidData, statesCovidData, statesYesterdayCovidData, globalHistorical, countryHistorical } from "./lib/covid-api.js";
 import minimist from 'minimist';
+import sql_operations from './lib/sql-operations.js';
 
 // set up server
 const app = express();
@@ -20,6 +23,9 @@ app.use(express.urlencoded({ extended: true }))  // support urlencoded besides J
 // get user input (terminal)
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000
+
+// set up sqlite3
+var sqlUtil = sql_operations;
 
 // timestamp difference helper
 async function timestampFormat(currentTs, updatedTs) {
@@ -52,6 +58,10 @@ app.get('/app/', async (req, res) => {
     /**
      * /app/ endpoint - Default Status Test
      */
+
+    // send logs
+    sqlUtil.insertLog("success", "/app/", 'no json involved');
+
     res.status(200);
     res.send('200 OK');
 })
@@ -64,8 +74,14 @@ app.get('/app/global/', async (req, res) => {
     globalAll().then(result => {
         res.status(200);
         if (args.j) {
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/", 'json flag');
+
             res.send(JSON.stringify(result, null, 4));
         } else {
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/", 'no json flag');
+
             let dt = new Date();
             
             res.send(`
@@ -81,8 +97,12 @@ app.get('/app/global/', async (req, res) => {
             Total Affected Countries: ${result['affectedCountries']}
             `)
         }
+
     })
     .catch(error => {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/global", "500 Internal server: error when retrieving data in .then() action.");
+
         res.status(500)
         res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
     })
@@ -96,9 +116,15 @@ app.get('/app/global/yesterday/', async (req, res) => {
     globalYesterdayAll().then(result => {
         res.status(200);
         if (args.j) {
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/yesterday/", 'json flag');
+
             res.send(JSON.stringify(result, null, 4));
         } else {
             let dt = new Date();
+
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/yesterday/", 'no json flag');
             
             res.send(`
             **Covid Global Data Result Yesterday**
@@ -115,6 +141,9 @@ app.get('/app/global/yesterday/', async (req, res) => {
         } 
     })
     .catch(error => {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/global", "500 Internal server: error when retrieving data in .then() action.");
+
         res.status(500)
         res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
     })
@@ -127,6 +156,9 @@ app.get('/app/country/', async (req, res) => {
 
     // validate country arg
     if (!args.c) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/country/", "400 BAD REQUEST: error due to user missing input");
+
         res.status(400);
         res.send("400 Bad Request\nThere is no specified country/region found in your input.\n")
     } else {
@@ -136,9 +168,12 @@ app.get('/app/country/', async (req, res) => {
                 /**
                 * It means the country not found in the API
                 */
+                
+                // send logs
+                sqlUtil.insertLog("failure", "/app/country/", '400 BAD REQUEST: error due to incorrect country input');
 
                 res.status(400);
-                
+
                 if (args.j) {
                     res.send(`400 Bad Request\nCountry/region \"${args.c}\" not found or doesn't have any cases.\n${JSON.stringify(result, null, 4)}`)
                 } else {
@@ -148,9 +183,15 @@ app.get('/app/country/', async (req, res) => {
             } else {
                 res.status(200);
                 if (args.j) {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/country/", 'json flag');
+
                     res.send(JSON.stringify(result, null, 4));
                 } else {
                     let dt = new Date();
+
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/country/", 'no json flag');
                     
                     res.send(`
                     **Covid Data Result of ${args.c}**
@@ -168,6 +209,9 @@ app.get('/app/country/', async (req, res) => {
             }
         })
         .catch(error => {
+            // send logs
+            sqlUtil.insertLog("failure", "/app/country/", '500 Internal server: error when retrieving data in .then() action.');
+            
             res.status(500)
             res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
         })
@@ -181,6 +225,9 @@ app.get('/app/country/yesterday/', async (req, res) => {
 
     // validate country arg
     if (!args.c) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/country/yesterday/", '400 BAD REQUEST: error due to user missing input');
+
         res.status(400);
         res.send("400 Bad Request\nThere is no specified country/region found in your input.\n")
     } else {
@@ -190,6 +237,9 @@ app.get('/app/country/yesterday/', async (req, res) => {
                 /**
                 * It means the country not found in the API
                 */
+
+                // send logs
+                sqlUtil.insertLog("failure", "/app/country/yesterday/", '400 BAD REQUEST: error due to incorrect country input');
 
                 res.status(400);
                 
@@ -202,8 +252,14 @@ app.get('/app/country/yesterday/', async (req, res) => {
             } else {
                 res.status(200);
                 if (args.j) {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/country/yesterday/", 'json flag');
+
                     res.send(JSON.stringify(result, null, 4));
                 } else {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/country/yesterday/", 'no json flag');
+
                     let dt = new Date();
                     
                     res.send(`
@@ -222,6 +278,9 @@ app.get('/app/country/yesterday/', async (req, res) => {
             }
         })
         .catch(error => {
+            // send logs
+            sqlUtil.insertLog("failure", "/app/country/yesterday/", '500 Internal server: error when retrieving data in .then() action.');
+
             res.status(500)
             res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
         })
@@ -235,6 +294,9 @@ app.get('/app/state/', async (req, res) => {
 
     // validate state arg
     if (!args.s) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/state/", '400 BAD REQUEST: error due to user missing input');
+
         res.status(400);
         res.send("400 Bad Request\nThere is no specified state in United States found in your input.\n")
     } else {
@@ -244,6 +306,9 @@ app.get('/app/state/', async (req, res) => {
                 /**
                 * It means the state not found in the API
                 */
+
+                // send logs
+                sqlUtil.insertLog("failure", "/app/state/", '400 BAD REQUEST: error due to incorrect state input');
 
                 res.status(400);
                 
@@ -256,8 +321,13 @@ app.get('/app/state/', async (req, res) => {
             } else {
                 res.status(200);
                 if (args.j) {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/state/", 'json flag');
+                    
                     res.send(JSON.stringify(result, null, 4));
                 } else {
+                    sqlUtil.insertLog("success", "/app/state/", 'no json flag');
+
                     let dt = new Date();
                     
                     res.send(`
@@ -275,6 +345,9 @@ app.get('/app/state/', async (req, res) => {
             }
         })
         .catch(error => {
+            // send logs
+            sqlUtil.insertLog("failure", "/app/state/", '500 Internal server: error when retrieving data in .then() action.');
+
             res.status(500)
             res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
         })
@@ -288,6 +361,9 @@ app.get('/app/state/yesterday/', async (req, res) => {
 
     // validate state arg
     if (!args.s) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/state/yesterday/", '400 BAD REQUEST: error due to user missing input');
+
         res.status(400);
         res.send("400 Bad Request\nThere is no specified state in United States found in your input.\n")
     } else {
@@ -297,6 +373,9 @@ app.get('/app/state/yesterday/', async (req, res) => {
                 /**
                 * It means the state not found in the API
                 */
+
+                // send logs
+                sqlUtil.insertLog("failure", "/app/state/yesterday/", '400 BAD REQUEST: error due to incorrect state input');
 
                 res.status(400);
                 
@@ -309,8 +388,14 @@ app.get('/app/state/yesterday/', async (req, res) => {
             } else {
                 res.status(200);
                 if (args.j) {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/state/yesterday/", 'json flag');
+
                     res.send(JSON.stringify(result, null, 4));
                 } else {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/state/yesterday/", 'no json flag');
+
                     let dt = new Date();
                     
                     res.send(`
@@ -328,6 +413,9 @@ app.get('/app/state/yesterday/', async (req, res) => {
             }
         })
         .catch(error => {
+            // send logs
+            sqlUtil.insertLog("failure", "/app/state/yesterday/", '500 Internal server: error when retrieving data in .then() action.');
+
             res.status(500)
             res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
         })
@@ -345,8 +433,14 @@ app.get('/app/global/historical/', async (req, res) => {
         res.status(200);
         let warning = args.d && pastDays < args.d ? "WARNING: The maximum days supported currently is 90\n" : "";  // Set a maximum days for historical data otherwise too long for json data.
         if (args.j) {
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/historical/", 'json flag');
+
             res.send(warning + JSON.stringify(result, null, 4));
         } else {
+            // send logs
+            sqlUtil.insertLog("success", "/app/global/historical/", 'no json flag');
+
             let dt = new Date();
             res.send(warning + `**Historical Covid Data (${pastDays} days)**\nRequest Time: ${dt.toUTCString()}\n`
             + JSON.stringify(result, null, 4)
@@ -354,6 +448,9 @@ app.get('/app/global/historical/', async (req, res) => {
         }
     })
     .catch(error => {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/global/historical/", '500 Internal server: error when retrieving data in .then() action.');
+
         res.status(500)
         res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
     })
@@ -366,6 +463,9 @@ app.get('/app/country/historical/', async (req, res) => {
      */
    
     if (!args.c) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/country/historical/", '400 BAD REQUEST: error due to user missing input');
+
         res.status(400);
         res.send("400 Bad Request\nThere is no specified country/region found in your input.\n")   
     } else {
@@ -376,6 +476,9 @@ app.get('/app/country/historical/', async (req, res) => {
                 /**
                 * It means the country not found in the API
                 */
+
+                // send logs
+                sqlUtil.insertLog("failure", "/app/country/historical/", '400 BAD REQUEST: error due to incorrect country input');
 
                 res.status(400);
 
@@ -389,8 +492,13 @@ app.get('/app/country/historical/', async (req, res) => {
                 res.status(200);
                 let warning = args.d && pastDays < args.d ? "WARNING: The maximum days supported currently is 90\n" : "";  // Set a maximum days for historical data otherwise too long for json data.
                 if (args.j) {
+                    // send logs
+                    sqlUtil.insertLog("success", "/app/country/historical/", 'json flag');
+
                     res.send(warning + JSON.stringify(result, null, 4));
                 } else {
+                    sqlUtil.insertLog("success", "/app/country/historical/", 'no json flag');
+
                     let dt = new Date();
                     res.send(warning + `**Historical Covid Data of ${args.c} (${pastDays} days)**\nRequest Time: ${dt.toUTCString()}\n`
                     + JSON.stringify(result, null, 4)
@@ -399,9 +507,56 @@ app.get('/app/country/historical/', async (req, res) => {
             }
         })
         .catch(error => {
+            // send logs
+            sqlUtil.insertLog("failure", "/app/country/historical/", '500 Internal server: error when retrieving data in .then() action.');
+
             res.status(500)
             res.send(`500 Internal Server Error\nThere is an error occured in the server when extracting data. Please try again later.\nError Log: ${error}`)
         })
+    }
+})
+
+app.get('/app/logs/', async (req, res) => {
+    /**
+     * get logs from the database (only 10 latest logs) to make better readability.
+     */
+
+    try {
+        let logs = sqlUtil.retrieveLog();
+
+        // send logs
+        sqlUtil.insertLog("success", "/app/logs/", 'dafault as json style');
+
+        res.status(200);
+        res.send(JSON.stringify(logs, null, 4));
+    } catch (err) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/logs/", '500 Internal server: error when retrieving logs from log database.');
+
+        res.status(500);
+        res.send(`500 Internal Server Error\nThere is an error occured in the server when retrieving data from log database. Please try again later.\nError Log: ${err}`)
+    }
+})
+
+app.get('/app/logs/clear/', async (req, res) => {
+    /**
+     * truncate/clear all the logs in the table.
+     */
+
+    try {
+        sqlUtil.truncateTableLog();
+
+        // send logs
+        sqlUtil.insertLog("success", "/app/logs/clear/", 'Successfully truncate/clear the table');
+
+        res.status(200);
+        res.send("Successfully truncated the log table in the database.");
+    } catch (err) {
+        // send logs
+        sqlUtil.insertLog("failure", "/app/logs/clear/", '500 Internal server: error when truncating logs from log database.');
+
+        res.status(500);
+        res.send(`500 Internal Server Error\nThere is an error occured in the server when truncating data from log database. Please try again later.\nError Log: ${err}`)
     }
 })
 
@@ -410,8 +565,12 @@ app.get('*', async (req, res) => {
      * all non-existent endpoints should return 404.
      */
 
+    // send logs
+    sqlUtil.insertLog("failure", "*", 'Visiting non-existent endpoint');
+
     res.status(404);
     res.send('404 NOT FOUND');
 })
 
+// listening to port
 app.listen(port);
